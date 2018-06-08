b86691bf24302eb762ab9e54975190f4734fced3
LeeHaeWon <banner4@cnu.ac.kr>
2018-06-08 15:07:32 +0900

    텀프로젝트2 / commit : 2 / SKeleton 완료. 찜목록 삭제

--- 3학년/웹프로그래밍/텀프로젝트/텀프2/mainpage.js
+++ 3학년/웹프로그래밍/텀프로젝트/텀프2/mainpage.js
@@ -1,17 +1,24 @@
 window.onload = function() {
   // 홈페이지 제목 (메인메뉴가기) 누르면.
   document.getElementById("main_header_click").onclick = function() {
-    location.href = "mainpage.html"; // mainpage로 이동.
+    location.href = "mainpage.html"; // 메인 페이지로 이동.
   }
-
   // 로그인 버튼 누르면.
   document.getElementById("LoginButton").onclick = function() {
-    location.href = "loginpage.html"; // loginpage로 이동.
+    location.href = "loginpage.html"; // 로그인 페이지로 이동.
   }
 
   // 사이즈 확인 버튼 누르면.
   document.getElementById("CheckSizeButton").onclick = function() {
-    location.href = "sizecheckpage.html"; // loginpage로 이동.
+    location.href = "sizecheckpage.html"; // 사이즈 확인 페이지로 이동.
   }
 
+  // 신발 별 착샷 버튼 누르면.
+  document.getElementById("InstgramButton").onclick = function() {
+    location.href = "searchpage.html"; // 신발 별 착샷 페이지로 이동.
+  }
+
+  document.getElementById("MakerHomepageButton").onclick = function() {
+    location.href = "makerhomepage.html"; // 제조사 홈페이지 링크 목록 페이지로 이동.
+  }
 }
